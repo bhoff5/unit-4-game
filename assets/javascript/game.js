ee95d7f2cfd4b4e4fef74c3f969c8fca23505664
Brian Hoff <blhoff52002@gmail.com>
2019-06-27 13:13:10 -0500

    uploading files

---
+++ assets/javascript/game.js
@@ -0,0 +1,74 @@
+$(document).ready(function () {
+    var magicNumber;
+    var wins = 0;
+    var losses = 0;
+    var crystal1Val;
+    var crystal2Val;
+    var crystal3Val;
+    var crystal4Val;
+    var pause = false;
+
+
+
+    function randomValues() {
+
+        magicNumber = Math.floor(Math.random() * 100) + 50;
+        crystal1Val = Math.floor(Math.random() * 15) + 1;
+        crystal2Val = Math.floor(Math.random() * 15) + 1;
+        crystal3Val = Math.floor(Math.random() * 15) + 1;
+        crystal4Val = Math.floor(Math.random() * 15) + 1;
+    }
+
+    function newGame() {
+
+        randomValues();
+        pause = false;
+        $("#magicNumber").text(magicNumber)
+        $("#crystalImage1").attr("data-crystalvalue", crystal1Val)
+        $("#crystalImage2").attr("data-crystalvalue", crystal2Val)
+        $("#crystalImage3").attr("data-crystalvalue", crystal3Val)
+        $("#crystalImage4").attr("data-crystalvalue", crystal4Val)
+    }
+    newGame()
+
+    function youWin() {
+        wins++
+        $("#wins").text(wins);
+    }
+
+    function youLose() {
+        $("#magicNumber").attr("id", "grayscale");
+        losses++;
+        $("#losses").text(losses);
+    }
+
+    $(".crystalImage").on("click", function () {
+        if (pause === false) {
+            var crystalVal = $(this).attr("data-crystalvalue");
+            magicNumber = magicNumber - parseInt(crystalVal);
+            $("#magicNumber").text(magicNumber);
+            if (magicNumber === 0) {
+                youWin()
+                console.log("you win")
+                pause = true;
+            }
+            else if (magicNumber < 0) {
+                youLose()
+                console.log("you lose")
+                pause = true;
+            }
+
+            else {
+                console.log("keep playing")
+            }
+        }
+
+    })
+
+    $("#playAgain").on("click", function () {
+        $("#grayscale").attr("id", "magicNumber");
+        newGame();
+    })
+
+
+})
